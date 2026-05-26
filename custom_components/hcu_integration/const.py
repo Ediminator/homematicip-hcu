d24ceb6b08443747dead168ab9c99cb7f03db510
philippA <erosiv01.begleitung@icloud.com>
2026-05-26 05:31:46 +0200

    Update plugin version to 2.0.0

--- custom_components/hcu_integration/const.py
+++ custom_components/hcu_integration/const.py
@@ -51,7 +51,7 @@ PLUGIN_FRIENDLY_NAME = {
     "de": "Home Assistant Integration",
     "en": "Home Assistant Integration",
 }
-PLUGIN_VERSION = "2.0.0.beta72"
+PLUGIN_VERSION = "2.0.0"
 PLUGIN_DOCUMENTATION_URL = "https://github.com/Ediminator/hacs-homematicip-hcu"
 PLUGIN_ISSUE_TRACKER_URL = "https://github.com/Ediminator/hacs-homematicip-hcu/issues"
 
