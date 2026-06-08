66717ad1dc380cb4dbef17ce697265a5e3e28564
philippA <erosiv01.begleitung@icloud.com>
2026-06-08 13:51:00 +0200

    Update const.py

--- custom_components/hcu_integration/const.py
+++ custom_components/hcu_integration/const.py
@@ -53,7 +53,7 @@ PLUGIN_FRIENDLY_NAME = {
     "de": "Home Assistant Integration",
     "en": "Home Assistant Integration",
 }
-PLUGIN_VERSION = "2.1.0"
+PLUGIN_VERSION = "2.1.0-beta1"
 PLUGIN_DOCUMENTATION_URL = "https://github.com/Ediminator/hacs-homematicip-hcu"
 PLUGIN_ISSUE_TRACKER_URL = "https://github.com/Ediminator/hacs-homematicip-hcu/issues"
 
